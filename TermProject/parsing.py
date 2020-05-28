863130165391d43852b249e546f29c2de1e30204
kyl990312 <56106969+kyl990312@users.noreply.github.com>
2020-05-29 01:35:17 +0900

    상세정보 접근을 위한 데이터 딕셔너리 생성

--- TermProject/parsing.py
+++ TermProject/parsing.py
@@ -1,30 +1,32 @@
 
 # -*- coding:utf-8 -*-
-from xml.dom.minidom import parseString
 import urllib.request
-url ='http://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=f69311384cfad095723cfa18c740c10b&svcType=api'
-response_body =None
+import urllib.parse
+from xml.etree import ElementTree
+def makeURL(str):
+    url ='http://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=f69311384cfad095723cfa18c740c10b&svcType=api' + str
+    return url
 
-def setURL(str):
-    str = '&svcCode=MAJOR&contentType=xml&gubun=univ_list'
-    global url
-    url += str
-
-def RequestXML():
-    global response_body, rescode
+def RequestXML(url):
     request = urllib.request.Request(url)
     response = urllib.request.urlopen(request)
     rescode = response.getcode()
     if rescode == 200:
-        response_body = response.read()
-        return response_body
+        return response.read()
     else:
         print("error code: "+rescode)
         return None
 
-def PrintXML():
-    print(response_body.decode('utf-8'))
-
-setURL('')
-RequestXML()
-PrintXML()
+def DataBuilder():
+    # list xml 파싱
+    url = makeURL('&svcCode=MAJOR&contentType=xml&gubun=univ_list')
+    listXmlFile = RequestXML(url)
+    #xml 문서에서 mClass와 majorSeq 추출
+    tree = ElementTree.fromstring(listXmlFile)
+    itemElements = tree.iter("content")
+    dictList = []
+    for item in itemElements:
+        mClass = item.find("mClass")
+        majorSeq = item.find("majorSeq")
+        dictList.append({mClass.text : majorSeq.text})
+    return dictList
