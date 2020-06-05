37bb8d1cd98899cb9120bd415ebd0c9615776ff1
JS-0125 <56055661+JS-0125@users.noreply.github.com>
2020-06-05 12:51:21 +0900

    board 중복 코드 수정

--- TermProject/board.py
+++ TermProject/board.py
@@ -1,96 +1,6 @@
-from tkinter import *
-<<<<<<< HEAD
-from loading import Data
-
-class MainGUI:
-    def __init__(self):
-        self.datas = Data()              # 검색하는 학과의 정보를 담는다.
-
-        self.window = Tk()
-        self.MajorFrame = Frame(self.window)
-        self.MajorFrame.grid(row = 0,column = 0)
-        self.UniFrame =Frame(self.window)
-        self.UniFrame.grid(row = 1,column = 0)
-        self.frame0 = Frame(self.window)
-        self.frame0.grid(row=0, column=1)
-        self.frames = []
-        # 학과 이름 입력
-        Label(self.MajorFrame, text="학과 이름", width=15).grid(row=0, column=0)
-        self.major = Entry(self.MajorFrame, width=15)
-        self.major.grid(row=1, column=0)
-        # 학과 이름 입력 후 오케이 버튼
-        Button(self.MajorFrame, text="OK", width=5, command= self.OKProcess).grid(row=1, column=1)
-
-        # 지역 분류 : 0.서울 특별시, 1. 수도권: 경기도 + 인천, 2. 충청도, 3. 강원도, 4. 전라도 + 광주 + 대전, 5. 경상도 + 대구 + 부산, 6.제주특별자치구
-        Label(self.UniFrame, text = "지역", width = 15).grid(row = 2, column =0)
-
-        self.checkVal0 = IntVar()
-        self.checkVal1 = IntVar()
-        self.checkVal2 = IntVar()
-        self.checkVal3 = IntVar()
-        self.checkVal4 = IntVar()
-        self.checkVal5 = IntVar()
-        self.checkVal6 = IntVar()
-        self.checkList = [Checkbutton(self.UniFrame, variable = self.checkVal0, width = 5, height = 1,  text = "서울"),
-                          Checkbutton(self.UniFrame, variable = self.checkVal1, width = 5, height = 1, text = "수도권"),
-                          Checkbutton(self.UniFrame, variable = self.checkVal2, width = 5, height = 1, text = "충청도"),
-                          Checkbutton(self.UniFrame, variable = self.checkVal3, width = 5, height = 1, text = "강원도"),
-                          Checkbutton(self.UniFrame, variable = self.checkVal4, width = 15, height = 1, text = "전라도,광주,대전"),
-                          Checkbutton(self.UniFrame, variable = self.checkVal5, width = 15, height = 1, text = "경상도,대구,부산"),
-                          Checkbutton(self.UniFrame, variable = self.checkVal6, width = 5, height = 1, text = "제주")]
-        self.checkList[0].grid(row = 3, column = 0)
-        self.checkList[1].grid(row=3, column=1)
-        self.checkList[2].grid(row=4, column=0)
-        self.checkList[3].grid(row=4, column=1)
-        self.checkList[4].grid(row=5, column=0)
-        self.checkList[5].grid(row=6, column=0)
-        self.checkList[6].grid(row=7, column=0)
-
-
-        # 학교 이름
-        Label(self.UniFrame, text="해당 과가 있는 학교", width=15,height = 1).grid(row=8, column=0)
-        self.listbox = Listbox(self.UniFrame,width = 15, height = 30)
-        self.listbox.grid(row = 9, column = 0)
-
-
-        # 프레임
-        # 프레임 1 : 학교 정보
-        # 프레임 2 : 학과 정보
-        # 프레임 3 : 취업 정보
-        for i in range(3):
-            Button(self.frame0, text="Frame" + str(i), command=lambda X=i: self.pressed(X)).pack(side=LEFT)
-            self.frames.append(Frame(self.window))
-            self.frames[i].grid(row=1, column=1)
-            label = Label(self.frames[i], text="FRAME" + str(i), width=60, height=30)
-            label.pack()
-
-        self.window.mainloop()
-
-    def pressed(self,X):
-        self.frames[X].tkraise()
-        if X == 0:
-            pass
-        if X == 1:
-            pass
-        if X == 2:
-            pass
-
-    def OKProcess(self):
-        self.datas.MakeUniversityData(self.major.get())              # 학과에 해당하는 대학정보를 만든다
-        self.InputUniVersityToList()
-
-    def InputUniVersityToList(self):
-        applicableList = []
-        idx = 0
-
-
-
-
-MainGUI()
-
-=======
 import map
 from loading import Data
+from tkinter import *
 
 def checkLogger():
     map.logger.setLevel(map._logging.INFO)
@@ -171,24 +81,6 @@ def getMap():
 
     m.save('D:/document/3-1/map.html')
 
-def setFrame():
-    # 학과 이름
-    Label(window, text="학과 이름", width=15).grid(row=0, column=0)
-    # 학과 이름 입력
-    Entry(window, width=15).grid(row=1, column=0)
-    # 학과 이름 입력 후 오케이 버튼
-    Button(window, text="OK", width=5).grid(row=1, column=1)
-    # 학교 이름
-    Label(window, text="해당 과가 있는 학교", width=15).grid(row=2, column=0)
-
-    # 프레임 1 : 학교 정보 // 프레임 2 : 학과 정보 // 프레임 3 : 취업 정보
-    for i in range(3):
-        Button(frame0, text="Frame" + str(i), command=lambda X=i: pressed(X)).pack(side=LEFT)
-        frames.append(Frame(window))
-        frames[i].grid(row=3, column=2)
-        label = Label(frames[i], text="FRAME" + str(i), width=10, height=10)
-        label.pack()
-
 def OKProcess(major):
     datas.MakeUniversityData(major.get())              # 학과에 해당하는 대학정보를 만든다
     InputUniVersityToList()
@@ -206,12 +98,8 @@ def pressed(X):
         toplevel.withdraw()
 
 def windowPlace():
-    width_window = 800
-    height_window = 600
-
     width_window = 1000
     height_window = 800
-
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
 
@@ -233,18 +121,6 @@ def windowPlace():
     toplevel.overrideredirect(1)
 
 if __name__ == '__main__':
-    checkLogger()
-    getMap()
-
-    window = Tk()
-    toplevel = Toplevel(window, width=window.winfo_width(), height=window.winfo_height())
-
-    windowPlace()
-
-    frame0 = Frame(window)
-    frame0.grid(row=1, column=2)
-    frames = []
-
     getMap()                    #지도 추출
 
     checkLogger()               #버전과 컴퓨터 환경 확인
@@ -269,4 +145,4 @@ if __name__ == '__main__':
     map.cef.Initialize()
     window.mainloop()
     map.cef.Shutdown()
->>>>>>> map
+
