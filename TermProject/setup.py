26972c8589bce81928622682a523e0ed1cf6c31c
kyl990312 <56106969+kyl990312@users.noreply.github.com>
2020-06-20 02:10:51 +0900

    발표영상및  ppt

---
+++ TermProject/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+
+setup(name = "JobSearching",
+      version= '1.0',
+      py_modules = ['board','brouser','Datas','graph','loading','mail','map','parsing','spam'])
